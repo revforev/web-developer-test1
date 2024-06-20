f1a4742c236161e589d3e76f90ede6aa1bf2b697
revforev <dutulescu_andrei@yahoo.com>
2024-06-20 09:24:10 +0100

    Fix JSON data issue in useGalleryData hook

--- lib/useGalleryData.ts
+++ lib/useGalleryData.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from 'react';
 
 const galleryJSON: string = `[
   { "filename": "Image-1.png", "altText": "Image One" },
@@ -8,7 +8,7 @@ const galleryJSON: string = `[
   { "filename": "Image-5.png", "altText": "Image Five" },
   { "filename": "Image-6.png", "altText": "Image Six" },
   { "filename": "Image-7.png", "altText": "Image Seven" },
-  { "filename": "Image-8.png", "altText": "Image Eight" },
+  { "filename": "Image-8.png", "altText": "Image Eight" }
 ]`;
 
 export interface GalleryImage {
@@ -21,9 +21,6 @@ export interface GalleryData {
 }
 
 export function useGalleryData(): GalleryData {
-
-  const [data] = useState<GalleryImage[]>(JSON.parse(galleryJSON) as GalleryImage[])
-
-  return { data }
-
+  const [data] = useState<GalleryImage[]>(JSON.parse(galleryJSON) as GalleryImage[]);
+  return {data};
 }
