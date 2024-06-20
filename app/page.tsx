0f15c87bb60d9eed00c0690362e0a4b7ad1171ae
revforev <dutulescu_andrei@yahoo.com>
2024-06-20 09:19:56 +0100

    first commit, forked repo and set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-'use client'
-import { useGalleryData } from "@/lib/useGalleryData";
+'use client';
+import {useGalleryData} from '@/lib/useGalleryData';
 
 export default function Home() {
   return (
     <main>
-      Add content and style according to https://www.figma.com/design/cqwIYXNHitVg2nAlMBfObA/Web-Developer-Test?node-id=0-1&t=nQJuZMDmryzLddLl-1
+      Add content and style according to
+      https://www.figma.com/design/cqwIYXNHitVg2nAlMBfObA/Web-Developer-Test?node-id=0-1&t=nQJuZMDmryzLddLl-1
     </main>
   );
 }
