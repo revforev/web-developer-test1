2223b0f38a246e78f84d3ddaca8ae2e8e1ff5a62
revforev <dutulescu_andrei@yahoo.com>
2024-06-21 11:42:53 +0100

    Style with precision, changed colours and gradient, changed Sort button according to design

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,34 @@ import {useState} from 'react';
 export default function Home() {
   const {data} = useGalleryData();
   const [images, setImages] = useState(data);
+  const [isSorted, setIsSorted] = useState(false);
 
   const handleSort = () => {
     setImages([...images].reverse());
+    setIsSorted(!isSorted);
   };
 
   return (
     <div className="page-container">
       <main>
-        <h1>Image Gallery</h1>
-        <button onClick={handleSort}>Reverse Order</button>
+        <h1>Web Developer Gallery</h1>
         <div className="gallery">
           {images.map((image, index) => (
             <div key={index} className="gallery-item">
               <div className="image-container">
-                <div className="image-block">
-                  <img src={`/img/${image.filename}`} alt={image.altText} />
-                </div>
+                <img src={`/img/${image.filename}`} alt={image.altText} />
               </div>
-              <p>{image.altText}</p>
             </div>
           ))}
         </div>
+        <button onClick={handleSort}>
+          Sort
+          <img
+            src={`/img/${isSorted ? 'IconUp.svg' : 'IconDown.svg'}`}
+            alt="Sort Icon"
+            className="sort-icon"
+          />
+        </button>
       </main>
       <style jsx>{`
         .page-container {
@@ -35,13 +41,13 @@ export default function Home() {
           justify-content: center;
           align-items: center;
           min-height: 100vh;
-          background-color: #f0f0f0;
+          background: linear-gradient(to top, #ff8ffc, #ffbffc);
           padding: 20px;
         }
         main {
           width: 100%;
           max-width: 1200px;
-          background-color: white;
+          background-color: #ffcdfc;
           padding: 20px;
           border-radius: 15px;
           box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
@@ -57,44 +63,42 @@ export default function Home() {
           text-align: center;
         }
         .image-container {
-          position: relative;
-          overflow: hidden;
-        }
-        .image-block {
           width: 100%;
-          padding-top: 100%;
-          position: relative;
-          border-radius: 10px;
+          height: auto;
         }
-        .image-block img {
-          position: absolute;
-          top: 0;
-          left: 0;
+        .image-container img {
           width: 100%;
-          height: 100%;
-          object-fit: cover;
-          border-radius: 10px;
+          height: auto;
+          display: block;
         }
         p {
           margin-top: 10px;
         }
         button {
-          margin-bottom: 20px;
+          display: inline-flex;
+          align-items: center;
+          margin-top: 20px;
           padding: 10px 20px;
           font-size: 16px;
           cursor: pointer;
-          background-color: #0070f3;
-          color: white;
+          background-color: white;
+          color: black;
           border: none;
-          border-radius: 5px;
+          border-radius: 25px;
           transition: background-color 0.3s ease;
         }
         button:hover {
           background-color: #005bb5;
         }
+        .sort-icon {
+          width: 16px;
+          height: 16px;
+          margin-left: 10px;
+        }
         h1 {
           margin-bottom: 20px;
-          text-align: center;
+          font-size: 3em;
+          text-align: left;
         }
       `}</style>
     </div>
